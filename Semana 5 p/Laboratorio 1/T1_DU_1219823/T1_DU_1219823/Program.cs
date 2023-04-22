44fbaacd74b2baa24964d162fc85d6169ad7e7a7
Urbina17 <123261313+Urbina17@users.noreply.github.com>
2023-04-22 11:10:11 -0600

    Add files via upload

---
+++ Semana 5 p/Laboratorio 1/T1_DU_1219823/T1_DU_1219823/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Mi segundo programa");
+        Console.WriteLine("Ingrese su nombre");
+        string Nombre = Console.ReadLine();
+        Console.WriteLine("Ingrese su edad");
+        string Edad = Console.ReadLine();
+        Console.WriteLine("Ingrese su carrera");
+        string Carrera = Console.ReadLine();
+        Console.WriteLine("Ingrese su carné");
+        string Carné = Console.ReadLine();
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("Edad: " + Edad);
+        Console.WriteLine("Carrera: " + Carrera);
+        Console.WriteLine(Carné);
+        Console.Write("Soy " + Nombre);
+        Console.Write(" tengo " + Edad);
+        Console.Write(" años y estudio la carrera de " + Carrera);
+        Console.Write(" ,mi número de carné es " + Carné);
+        Console.ReadKey();
+
+
+    }
+}
